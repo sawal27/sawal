6204bf133e45c65dfe60d83a0f0cf9639b32a639
sawal27 <36143663+sawal27@users.noreply.github.com>
2018-02-05 13:22:33 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+
+<title>WEB DESIGN 1</title>
+</head>
+<body style="background-color:gray;">
+
+<center><h1 style="background-color:LightGray;">PERSONAL DATA</h1></center>
+<p><h2>NAME:<u>Alenie Sawal</u></h2></p>
+<p><h2>AGE N SEX:<u>19 Female</u></h2></p>
+<p><h2>ADDRESS:<u>St.Cadulawan Minglanilla Cebu</u></h2></p>
+<p><h2>DATE OF BIRTH:<u>September 27,1998</u></h2><p>
+<p><h2>CIVIL STATUS:<u>Single</u></h2></p>
+<p><h2>RELIGION:<u>Roman Catholic</u></h2></p>
+<p><h2>CITIZENSHIP:<u>Filipino</u></h2></p>
+<p><h2>EMAIL ADDRESS:<u>alenie.sawal@gmail.com</u></h2></p><hr>
+<center><h1 style="background-color:LightGray">EDUCATIONAL ATTAINMENT</h1></center>
+<p><h2>ELEMENTARY:<u>Lourdes elementary school</u></h2></p>
+<p><h2>HIGH SCHOOL:<u>Sta.maria high school minalin</u></h2></p>
+<h1><center>MY BIOGRAPHY<\center><\h1>
+<p><h3>I am Alenie sawal,19 years old of age and now living in minglanillia cebu.
+I was graduated elementary in lourdes elementary school minalin pampanga
+graduated highschool in Sta.maria highschool minalin pampanga.
+I am the eldest among four siblings,my mother's name is Jenely flores Sawal,
+my father's name is Alexander Sawal,last year i decided to back in studying.
+I want to finish my study,because i want to get a job,to become a 
+professional to make proud my parents,and to help my siblings to
+finish their study to become a professional to.</h3></p>
+
+
+</body>
+</html>
